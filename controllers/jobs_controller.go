7874be1096d9babf7b0261ef131e863bb5d0db64
learnin <i.learnin@gmail.com>
2015-05-10 02:27:20 +0900

    Implement feature

--- controllers/jobs_controller.go
+++ controllers/jobs_controller.go
@@ -15,7 +15,7 @@ type JobsController struct {
 	Logger *multilog.MultiLogger
 }
 
-type ReqForm struct {
+type Request struct {
 	Async         bool
 	RequireResult bool
 	Command       string
@@ -29,10 +29,19 @@ func (controller *JobsController) Show(c web.C, w http.ResponseWriter, r *http.R
 }
 
 func (controller *JobsController) Run(c web.C, w http.ResponseWriter, r *http.Request) {
-	form := ReqForm{}
-	if err := json.NewDecoder(r.Body).Decode(&form); err != nil {
+	req := Request{}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		controller.Logger.Error(err)
 		http.Error(w, "", http.StatusBadRequest)
 		return
+	}
+	if req.Async && req.RequireResult {
+		db := controller.DS.GetDB()
+		if d := db.Exec("insert into jobs(async, require_result, command, args) VALUES(?, ?, ?, ?)", req.Async, req.RequireResult, req.Command, req.Args); d.Error != nil {
+			controller.Logger.Errorf("jobs テーブル登録時にエラーが発生しました。error=%v", d.Error)
+			sendEroorResponse(w, d.Error, "")
+			return
+		}
 	}
 	encoder := json.NewEncoder(w)
 	encoder.Encode(response{Error: false, Messages: []string{}})
